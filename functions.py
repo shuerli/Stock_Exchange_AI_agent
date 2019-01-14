a177b021f2a1a484231f83833fd438e70634febe
TrueNo <mfb539@hotmail.com>
2019-01-13 22:28:53 -0500

    put all functions in separate file

---
+++ functions.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import backtest as twp
+import numpy as np
+from matplotlib import pyplot as plt
+import random, timeit
+from sklearn import preprocessing
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import load_model
+
+
+# pnl explained : https://www.investopedia.com/ask/answers/how-do-you-calculate-percentage-gain-or-loss-investment/
+# backtesting: a strategy to analyze how accurate a model did in performing trading with historycal data
+def getData():
+    price = pd.read_csv('csv/FB1min1000.csv')
+    # price = price.tail(100).reset_index()
+    price = price[900:1400]
+    price = price.reset_index()
+    price = price['4. close']
+
+    price2 = pd.read_csv('csv/FB1min1000.csv')
+    price2 = price2[899:1399]
+    price2 = price2.reset_index()
+    price2 = price2['4. close']
+
+    sma20 = pd.read_csv('csv/FB1min1000sma20.csv')
+    # sma20 = sma20.tail(100).reset_index()
+    sma20 = sma20[900:1400]
+    sma20 = sma20.reset_index()
+    sma20 = sma20['SMA']
+    sma80 = pd.read_csv('csv/FB1min1000sma80.csv')
+    # sma80 = sma80.tail(100).reset_index()
+    sma80 = sma80[900:1400]
+    sma80 = sma80.reset_index()
+    sma80 = sma80['SMA']
+    return price, price2, sma20, sma80
+
+
+def initializeState(data, data_prev, sma20, sma80):
+    # Preprocessing Data
+    pdata = np.column_stack((data, data_prev, sma20, sma80))
+    pdata = np.nan_to_num(pdata)
+    scaler = preprocessing.StandardScaler()
+    pdata = scaler.fit_transform(pdata)
+    pdata = np.expand_dims(scaler.fit_transform(pdata), axis=1)  # add dimension for lstm input
+    initialState = pdata[1:2, :, :]
+
+    return initialState, pdata
+
+
+def trade(action, pdata, signal, timeStep, inventory, data, totalProfit):
+    profit = totalProfit
+    timeStep += 1
+    state = pdata[timeStep: timeStep + 1, :, :]  # preserves dimension for lstm input
+    if timeStep + 1 == pdata.shape[0]:
+        endState = 1
+    else:
+        endState = 0
+
+    if action == 1:  # buy 1
+        signal.loc[timeStep - 1] = 1
+        inventory.append(data[timeStep - 1])
+    elif action == 2:  # and len(inventory) > 0: #sell 1
+        signal.loc[timeStep - 1] = -1
+        if len(inventory) > 0:
+            profit += data[timeStep - 1] - inventory.pop(0)
+    else:
+        signal.loc[timeStep - 1] = 0
+
+    return state, timeStep, signal, endState, profit
+
+
+def getReward(timeStep, signal, endState, price):
+    net = (price[timeStep] - price[timeStep - 1]) * signal[timeStep - 1]
+    rewards = 0
+    if not endState:
+
+        if net > 0:
+            rewards = 1
+        elif net < 0:
+            rewards = -1
+
+    else:
+        bt = twp.Backtest(price, signal, signalType='shares')
+        rewards = bt.pnl.iloc[-1]
+
+    return rewards
+
+
+def getModel():
+    num_features = 4
+    exist = 0
+
+    if exist:
+        model = load_model('model/episode400.h5')
+    else:
+        model = Sequential()
+        model.add(LSTM(80, input_shape=(1, num_features), return_sequences=True, stateful=False))
+        model.add(Dropout(0.2))
+        model.add(LSTM(80, return_sequences=False, stateful=False))
+        model.add(Dropout(0.2))
+        model.add(Dense(3, activation="linear"))
+        model.compile(optimizer='adam', loss='mse')
+    return model
+
+
+def test(model, data,data_prev, sma20, sma80):
+    # This function is used to evaluate the performance of the system each epoch, without the influence of epsilon and random actions
+    signal = pd.Series(index=np.arange(len(data)))
+    signal.fillna(value=0, inplace=True)
+
+    state, pdata = initializeState(data, data_prev, sma20, sma80)
+    endState = 0
+    timeStep = 1
+    realProfit = 0
+    realInventory = []
+    while not endState:
+        Q = model.predict(state, batch_size=1)
+        action = (np.argmax(Q))
+        # print(Q,'***',action)
+        nextState, timeStep, signal, endState, realProfit = trade(action, pdata, signal, timeStep, realInventory, data,
+                                                                  realProfit)
+
+        state = nextState
+    while len(realInventory) > 0:
+        realProfit += data.iloc[-1] - realInventory.pop(0)
+
+    long = 0
+    short = 0
+    hold = 0
+    for i in range(signal.shape[0]):
+        if signal.iloc[i] < 0:
+            short += 1
+        elif signal.iloc[i] > 0:
+            long += 1
+        else:
+            hold+=1
+    print('r-Buy: ', long, ', r-Sell: ', short, 'r-hold: ',hold)
+
+    bt = twp.Backtest(data, signal, signalType='shares')
+    endReward = bt.pnl.iloc[-1]
+
+    return endReward, realProfit
--- train.py
+++ train.py
@@ -6,139 +6,12 @@ import random, timeit
 from sklearn import preprocessing
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import load_model
 
 
-# pnl explained : https://www.investopedia.com/ask/answers/how-do-you-calculate-percentage-gain-or-loss-investment/
-#backtesting: a strategy to analyze how accurate a model did in performing trading with historycal data
-def getData():
-    price = pd.read_csv('csv/bull_test/GOOG1min100.csv')
-    #price = price.tail(100).reset_index()
-    price = price[900:1400]
-    price = price.reset_index()
-    price = price['4. close']
-    
-    price2 = pd.read_csv('csv/bull_test/GOOG1min100.csv')
-    price2 = price2[899:1399]
-    price2 = price2.reset_index()
-    price2 = price2['4. close']
-    
-    sma20 = pd.read_csv('csv/bull_test/GOOG1min100sma20.csv')
-    #sma20 = sma20.tail(100).reset_index()
-    sma20 = sma20[900:1400]
-    sma20 = sma20.reset_index()
-    sma20 = sma20['SMA']
-    sma80 = pd.read_csv('csv/bull_test/GOOG1min100sma80.csv')
-    #sma80 = sma80.tail(100).reset_index()
-    sma80 = sma80[900:1400]
-    sma80 = sma80.reset_index()
-    sma80 = sma80['SMA']
-    return price,price2, sma20,sma80
-
-def initializeState(data,data_prev, sma20, sma80):
-    # Preprocessing Data
-    pdata = np.column_stack((data, data_prev, sma20, sma80))
-    pdata = np.nan_to_num(pdata)
-    scaler = preprocessing.StandardScaler()
-    pdata = scaler.fit_transform(pdata)
-    pdata = np.expand_dims(scaler.fit_transform(pdata), axis=1) #add dimension for lstm input
-    initialState = pdata[1:2,:,:]
-
-
-    return initialState, pdata
-
-def trade(action, pdata, signal, timeStep,inventory,data,totalProfit):
-    profit = totalProfit
-    timeStep += 1
-    state = pdata[timeStep: timeStep + 1,:,:]  # preserves dimension for lstm input
-    if timeStep +1 == pdata.shape[0]:
-        endState = 1
-    else:
-        endState = 0
-
-    if action == 1: #buy 1
-        signal.loc[timeStep-1] = 1
-        inventory.append(data[timeStep-1])
-    elif action == 2:# and len(inventory) > 0: #sell 1
-        signal.loc[timeStep-1] = -1
-        if len(inventory) > 0:
-            profit += data[timeStep-1] - inventory.pop(0)
-    else:
-        signal.loc[timeStep-1] = 0
-
-    return state, timeStep, signal, endState, profit
-
-def getReward(timeStep, signal, endState, price):
-
-    net = (data[timeStep] - data[timeStep-1])*signal[timeStep-1]
-    rewards = 0
-    if not endState:
-        
-        if net >0:
-            rewards = 1
-        elif net < 0:
-            rewards = -1
-
-    else:
-        bt = twp.Backtest(price, signal, signalType='shares')
-        rewards = bt.pnl.iloc[-1]
-        
-    return rewards
-
-def getModel():
-
-    num_features = 4
-    exist =  0
-
-    if exist:
-        model = load_model('model/episode400.h5')
-    else:
-        model = Sequential()
-        model.add(LSTM(80, input_shape=(1, num_features), return_sequences=True, stateful=False))
-        model.add(Dropout(0.2))
-        model.add(LSTM(80, return_sequences=False, stateful=False))
-        model.add(Dropout(0.2))
-        model.add(Dense(3, activation="linear"))
-        model.compile(optimizer='adam', loss='mse')
-    return model
-
-def test(model,data, sma20, sma80):
-    # This function is used to evaluate the performance of the system each epoch, without the influence of epsilon and random actions
-    signal = pd.Series(index=np.arange(len(data)))
-    signal.fillna(value=0, inplace=True)
-
-    state, pdata = initializeState(data,data_prev, sma20, sma80)
-    endState = 0
-    timeStep = 1
-    realProfit = 0
-    realInventory = []
-    while not endState:
-        Q = model.predict(state, batch_size=1)
-        action = (np.argmax(Q))
-        #print(Q,'***',action)
-        nextState, timeStep, signal, endState, realProfit = trade(action, pdata, signal, timeStep, realInventory, data,realProfit)
-
-        state = nextState
-    while len(realInventory) > 0:
-        realProfit += data.iloc[-1] - realInventory.pop(0)
-
-    long = 0
-    short = 0
-    for i in range(signal.shape[0]):
-        if signal.iloc[i] < 0:
-            short += 1
-        elif signal.iloc[i] > 0:
-            long += 1
-    print('r-Buy: ',long,', r-Sell: ',short)
-
-    bt = twp.Backtest(data, signal, signalType='shares')
-    endReward = bt.pnl.iloc[-1]
-
-    return endReward,realProfit
-
-# Main program start
+from functions import *
 
 episodes = 500
 gamma = 0.95  # higher gamma, more important future reward
@@ -227,19 +100,22 @@ for i in range(episodes):
 
     long = 0
     short = 0
+    hold = 0
     for j in range(signal.shape[0]):
         if signal.iloc[j] < 0:
             short += 1
         elif signal.iloc[j] > 0:
             long += 1
-    print('Buy: ', long, ', Sell: ', short)
+        else:
+            hold+=1
+    print('Buy: ', long, ', Sell: ', short, ', Hold: ',hold)
 
     bt = twp.Backtest(data, signal, signalType='shares')
     endReward = bt.pnl.iloc[-1]
     plt.figure(figsize=(20, 10))
 
 
-    r_reward, r_profit = test(model,data,sma20,sma80)
+    r_reward, r_profit = test(model,data,data_prev,sma20,sma80)
 
     real_pnl_progress.append((r_reward))
     real_profit_progress.append((r_profit))
@@ -251,16 +127,16 @@ for i in range(episodes):
     bt.plotTrades()
  
     plt.suptitle(str(i))
-    plt.savefig('plots/bull_test/' + str(i) + '.png')
+    plt.savefig('plot/' + str(i) + '.png')
     plt.show()
 
     if i % 20 == 0:
-        model.save('model/bull_test/episode'+str(i)+'.h5')
+        model.save('model/episode'+str(i)+'.h5')
 
 elapsed = np.round(timeit.default_timer() - startTime, decimals=2)
 print("Completed in %f" % (elapsed,))
 
-model.save('model/bull_test/FINAL_model.h5')
+model.save('model/FINAL_model.h5')
 bt = twp.Backtest(data, signal, signalType='shares')
 bt.data['delta'] = bt.data['shares'].diff().fillna(0)
 
@@ -301,5 +177,5 @@ plt.xlabel("Episode(s)")
 plt.plot(profit_df,'b')
 plt.plot(r_profit_df,'r')
 plt.tight_layout()
-plt.savefig('plots/bull_test/summary' + '.png', bbox_inches='tight', pad_inches=1, dpi=72)
+plt.savefig('plot/summary' + '.png', bbox_inches='tight', pad_inches=1, dpi=72)
 plt.show()
